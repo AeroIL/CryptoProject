fcc5fcb1367be800059cf403e06d96bdca8f9a80
The Michel <przeusil@gmail.com>
2021-12-07 04:19:07 +0200

    commit on 'main'

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 let coinArr = [];
 let currecy = [];
+let favCoins = [];
+let rev = 0;
 function coinsP() {
-  $.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100
-  `, (coins) => {
-    console.log(coins);
-    coinArr = coins;
-    displayCoins();
-  });
+  $.get(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100
+  `,
+    (coins) => {
+      console.log(coins);
+      coinArr = coins;
+      displayCoins();
+    }
+  );
 }
 function displayCoins() {
   const coinsFilter = coinArr.filter((token) =>
     token.symbol.includes($(".cSearch").val().toLowerCase())
   );
 
-
-    for (const token of coinsFilter) {
+  for (const token of coinsFilter) {
     $(".main").append(`<div class="cBox">
  
     <h1 class="cName">${token.name}</h1>
@@ -24,16 +28,43 @@ function displayCoins() {
     <button class="cInfo">More Information</button>
     <div class="cBoxMore">   
     <img src="${token.image}" class="cIMG">
-    <p style="font-weight:bold">${parseInt(token.current_price).toFixed(2)}$</p>
-    <p>${parseInt(token.current_price*currecy.USDEUR).toFixed(2)}€</p>
-    <p>${parseInt(token.current_price*currecy.USDILS).toFixed(2)}₪</p>
+    <p style="font-weight:bold">${parseFloat(token.current_price).toFixed(
+      5
+    )}$</p>
+    <p>${parseFloat(token.current_price * currecy.USDEUR).toFixed(5)}€</p>
+    <p>${parseFloat(token.current_price * currecy.USDILS).toFixed(5)}₪</p>
     </div>
 </div>
     `);
-  }$(".cInfo").click(function() {
-    $(this).next().slideToggle()
-  });
   }
+  $(".cInfo").click(function  () {
+    $(this).next().slideToggle();
+    if(rev === 0){
+    $(".main").height( $(".main").height()+40);
+    rev++;
+  }
+  else{
+    $(".main").height( $(".main").height()-40);
+    rev = 0;
+  }
+  });
+  $(".cToggle").click(function () {
+    if(favCoins.length < 5){
+    if (this.checked) {
+      console.log($(this).prev().prev().text());
+      
+      favCoins.push($(this).prev().prev().text());
+      console.log(favCoins);
+    } else {
+      favCoins.splice($(this).prev().prev().text().indexOf(), 1);
+      console.log(favCoins);
+    }
+  }
+  else{
+    $(".cToggle").prop("disabled", true);
+  }
+  });
+}
 $(".about").click(function () {
   $(".main").html("");
 });
@@ -47,9 +78,12 @@ $(".cSearch").keyup(function () {
   displayCoins();
 });
 function Fixer() {
-  $.get(`http://api.currencylayer.com/live?access_key=03cffa3d589b534c9e6b49de12678fd5&format=1`, (eurTo) => {
-    console.log(eurTo);
-    currecy = eurTo.quotes;
-});
+  $.get(
+    `http://api.currencylayer.com/live?access_key=03cffa3d589b534c9e6b49de12678fd5&format=1`,
+    (eurTo) => {
+      console.log(eurTo);
+      currecy = eurTo.quotes;
+    }
+  );
 }
 Fixer();
