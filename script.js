65a52bfed1b150fa310331263bdafaf54f91ff48
The Michel <przeusil@gmail.com>
2021-12-30 17:24:44 +0200

    commit on main

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 $(() => {
   let coinArr = [];
-  let favCoins = !localStorage.Favorites ? []: JSON.parse(localStorage.Favorites);
-  
+  let favCoins = !localStorage.Favorites
+    ? []
+    : JSON.parse(localStorage.Favorites);
+  let CoinsInfo = !localStorage.Info ? {} : JSON.parse(localStorage.Info);
+
   let newFav;
   function coinsP() {
     $.get(
@@ -14,17 +17,30 @@ $(() => {
       }
     );
   }
-  function displayCoins() {
+  function localStorageset(name, usd, ils, eur, img) {
+    CoinsInfo[name] = {
+      usd,
+      ils,
+      eur,
+      img,
+      Time: Date.now(),
+    };
+    localStorage.setItem("Info", JSON.stringify(CoinsInfo));
+  }
 
+  function displayCoins() {
     if ($(".cSearch").val() === "") {
       for (const token of coinArr) {
         $(".main").append(`<div class="cBox">
    
       <h1 class="cName">${token.name}</h1>
       <p class="cSym">${token.symbol.toUpperCase()}</p>
-     <input type="checkbox" id="${token.name}" class="cToggle">
+     <input type="checkbox" id="${token.name
+       .toLowerCase()
+       .replaceAll(" ", "")
+       .replaceAll(".", "_")}" class="cToggle">
       
-      <button class="cInfo">More Information</button>
+      <button id="${token.id}" class="cInfo">More Information</button>
       <div class="cBoxMore">   
       <div class="loader-container">
         <div class="loader"></div>
@@ -33,7 +49,9 @@ $(() => {
   </div>
       `);
         $(".loadersc").remove();
-        checkbox(token.name);
+        checkbox(
+          token.name.toLowerCase().replaceAll(" ", "").replaceAll(".", "_")
+        );
       }
     } else {
       const coinsFilter = coinArr.filter(
@@ -41,13 +59,17 @@ $(() => {
       );
 
       for (const token of coinsFilter) {
-        $(".main").append(`<div class="cBox">
+        if ($(".cSearch").val().toLowerCase() === token.symbol) {
+          $(".main").append(`<div class="cBox">
  
     <h1 class="cName">${token.name}</h1>
     <p class="cSym">${token.symbol.toUpperCase()}</p>
    
-    <input type="checkbox" id="${token.name}" class="cToggle">
-    <button class="cInfo">More Information</button>
+    <input type="checkbox" id="${token.name
+      .toLowerCase()
+      .replaceAll(" ", "")
+      .replaceAll(".", "_")}" class="cToggle">
+    <button id="${token.id}" class="cInfo">More Information</button>
     <div class="cBoxMore"> 
     <div class="loader-container">
     <div class="loader"></div>
@@ -56,46 +78,93 @@ $(() => {
    
 </div>
     `);
-    checkbox(token.name);
-  }
+          checkbox(
+            token.name.toLowerCase().replaceAll(" ", "").replaceAll(".", "_")
+          );
+        } else {
+          $(".main").html(`
+    <h1 class="e404">Try again...</h1>
+    `);
+        }
+      }
     }
     $(".cInfo").click(function () {
+      let coinID = $(this).attr("id")
+      console.log(coinID)
       $(this).next().slideToggle();
-      $.get(
-        `https://api.coingecko.com/api/v3/coins/
-    `,
-        (coins) => {
-          const moreInfo = coins.filter(
-            (token) => token.name === $(this).siblings(".cName").text()
-          );
-          $(this).next().html(` <img src="${
-            moreInfo[0].image.large
-          }" class="cIMG">
-      <p style="font-weight:bold">${parseFloat(
-        moreInfo[0].market_data.current_price.usd
-      )}$</p>
-      <p>${parseFloat(moreInfo[0].market_data.current_price.eur)}€</p>
-      <p>${parseFloat(moreInfo[0].market_data.current_price.ils)}₪</p>
-      `);
-        }
-      );
+      if (
+        CoinsInfo[$(this).siblings(".cName").text()] &&
+        Date.now() - CoinsInfo[$(this).siblings(".cName").text()].Time < 120000
+      ) {
+        $(this).next().html(` <img src="${
+          CoinsInfo[$(this).siblings(".cName").text()].img
+        }" class="cIMG">
+    <p style="font-weight:bold">${parseFloat(
+      CoinsInfo[$(this).siblings(".cName").text()].usd
+    )}$</p>
+    <p>${parseFloat(CoinsInfo[$(this).siblings(".cName").text()].eur)}€</p>
+    <p>${parseFloat(CoinsInfo[$(this).siblings(".cName").text()].ils)}₪</p>
+    `);
+      } else {
+        $.get(
+          `https://api.coingecko.com/api/v3/coins/${coinID}
+      `,
+          (coin) => {
+            let coinName = $(this).siblings(".cName").text();
+            let usdPrice = coin.market_data.current_price.usd;
+            let nisPrice = coin.market_data.current_price.ils;
+            let eurPrice = coin.market_data.current_price.eur;
+            let imgInfo = coin.image.large;
+            localStorageset(coinName, usdPrice, nisPrice, eurPrice, imgInfo);
+
+            $(this).next().html(` <img src="${
+              coin.image.large
+            }" class="cIMG">
+        <p style="font-weight:bold">${parseFloat(
+          coin.market_data.current_price.usd
+        )}$</p>
+        <p>${parseFloat(coin.market_data.current_price.eur)}€</p>
+        <p>${parseFloat(coin.market_data.current_price.ils)}₪</p>
+        `);
+          }
+        );
+      }
     });
+
     $(".cToggle").change(function () {
-      if (favCoins.length < 5 || !$(this).prop( "checked" )) {
-        if ($(this).prop( "checked" )) {
-          favCoins.push($(this).prev().prev().text());
-          localStorage.setItem("Favorites", JSON.stringify(favCoins));
-          console.log(favCoins)
+      if (favCoins.length < 5 || !$(this).prop("checked")) {
+        if ($(this).prop("checked")) {
           
+          favCoins.push($(this)
+          .prev()
+          .prev()
+          .text()
+          .toLowerCase()
+          .replaceAll(" ", "")
+          .replaceAll(".", "_"));
+          console.log(favCoins);
+          localStorage.setItem("Favorites", JSON.stringify(favCoins));
+          console.log(favCoins);
         } else {
-          let indexFav = favCoins.indexOf($(this).prev().prev().text());
+          let indexFav = favCoins.indexOf($(this)
+          .prev()
+          .prev()
+          .text()
+          .toLowerCase()
+          .replaceAll(" ", "")
+          .replaceAll(".", "_"));
           favCoins.splice(indexFav, 1);
 
           localStorage.setItem("Favorites", JSON.stringify(favCoins));
-      
         }
       } else {
-        newFav = $(this).prev().prev().text();
+        newFav = $(this)
+          .prev()
+          .prev()
+          .text()
+          .toLowerCase()
+          .replaceAll(" ", "")
+          .replaceAll(".", "_");
         $("body").append(`
         
         <div class="popDiv">
@@ -111,20 +180,23 @@ $(() => {
       function favC() {
         for (const favCoin of favCoins) {
           $(".popDiv").append(`<div id="favdata">
-    <h3 class="popCoin">${favCoin}</h3>
+    <h3 class="popCoin">${
+      favCoin.charAt(0).toUpperCase() + favCoin.slice(1)
+    }</h3>
     <button class="popCdel"> 🗑 </button>
     </div>
     `);
         }
       }
-   
+
       $(".popCdel").click(function dltCoin() {
-        let indexFav = favCoins.indexOf($(this).prev().text());
+        let indexFav = favCoins.indexOf($(this).prev().text().toLowerCase());
         favCoins.splice(indexFav, 1);
-        favCoins.push(newFav);
+        favCoins.push(
+          newFav.toLowerCase().replaceAll(" ", "").replaceAll(".", "_")
+        );
         localStorage.setItem("Favorites", JSON.stringify(favCoins));
         console.log(favCoins);
-         displayCoins();
         $(".popDiv").remove();
         $(".blackbg").remove();
         $(".main").html("");
@@ -132,20 +204,18 @@ $(() => {
       });
     });
   }
-  console.log(favCoins)
+  console.log(favCoins);
   function checkbox(e) {
     for (const coin of favCoins) {
-      console.log(coin === e)
+      console.log(coin === e);
       if (coin === e) {
-        $(`#${e}`).prop("checked",true);
-   
+        $(`#${e}`).prop("checked", true);
       } else {
         // $(`#${e}`).removeAttr('checked');
-        
       }
     }
   }
-  
+
   $(".about").click(function () {
     $(".main").html("");
     $(".main").append(`
@@ -154,10 +224,10 @@ $(() => {
   </div>
   <div class="aboutDiv">
   <h1>Michel Gomon ,23 ,From Lod,Israel.</h1>
-  <h3>Basically the site is writen with : Html, Css, Jquery.</h3>
-  <h3>This Project is about the Crypto World, It's shows you the coin prices per 3 currncies (USD,EUR,ILS).</h3>
-  <h3>You can add up to 5 coins to your favorites, The coins that selected to your favorites will be displayed ...</h3>
-  <h3>with a yellow star</h3>
+  <h3>Basically the site is writen with : Html, Css, Jquery.
+  This Project is about the Crypto World, It's shows you the coin prices per 3 currncies (USD,EUR,ILS).
+  You can add up to 5 coins to your favorites, The coins that selected to your favorites will be displayed ...
+  with a yellow star,When you want to look for more information it will update every 2 minutes,when you press the button the data will store in the localstorage </h3>
   </div>
   `);
   });
